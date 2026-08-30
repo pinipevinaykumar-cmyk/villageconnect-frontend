eded99ba937583ad3b5780d217932c59a3d56d7a
pinipevinaykumar-cmyk <pinipevinaykumar@gmail.com>
2026-08-30 21:52:17 +0530

    Hardcode backend URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL
-    ? `${process.env.REACT_APP_API_URL}/api`
-    : 'http://localhost:8080/api',
+  baseURL: 'https://villageconnect-backend-production.up.railway.app/api',
 });
 
 API.interceptors.request.use((config) => {
